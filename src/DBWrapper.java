033899c1ccd6ff280a1832a802342d7150eda4d9
Ilya Sysoi <14ilya12322@gmail.com>
2018-05-29 00:04:07 +0300

    <<menu>>: add database wrapper

---
+++ src/DBWrapper.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+
+public class DBWrapper {
+    // class to fetch some info from bd
+
+
+    public void operationWitInfo(Menu.ChangeInfoType operationType,
+                                 Menu.InfoType infoType) {
+
+    }
+
+    public FacultyInfo[] getFacultiesInfo() {
+
+        FacultyInfo[] facult = new FacultyInfo[5];
+
+        return facult;
+    }
+
+
+}
