c842364a08aa3172dd153a623f542659d25ea097
Ilya Sysoi <14ilya12322@gmail.com>
2018-05-28 21:23:03 +0300

    <<init project>>: add menu for choose user type

--- src/Menu.java
+++ src/Menu.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
+public class Menu {
 
-public class Menu
-{
+    enum UserType {
+        administrator,
+        user
+    }
+
+    private static UserType getUserType() {
+        System.out.println("Выберите тип пользователя: ");
+        System.out.println("[1] - администратор");
+        System.out.println("[2] - абитуриент");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        if ("1".equals(input)) {
+            return UserType.administrator;
+        }
+        return UserType.user;
+    }
+
     public static void main(String [] args) {
-        System.out.println("test");
+        UserType userType = getUserType();
     }
 
 }
