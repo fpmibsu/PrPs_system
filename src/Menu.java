feeb4870cdaefb20cbaf79f50fb9b89f26a2575b
Ilya Sysoi <14ilya12322@gmail.com>
2018-05-28 23:01:59 +0300

    <<init project>>: add templeate of menu for both user types

--- src/Menu.java
+++ src/Menu.java
@@ -21,8 +21,67 @@ public class Menu {
         return UserType.user;
     }
 
+
+    private static void administratorMenu() {
+        while (true) {
+            System.out.println("------администратор------");
+            System.out.println("Выберите действие: ");
+            System.out.println("[1] - добавиить информацию");
+            System.out.println("[2] - удалить информацию");
+            System.out.println("[3] - обновить информацию");
+            System.out.println("[0] - выход");
+
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            switch (input){
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+            }
+            if ("0".equals(input)) {
+                break;
+            }
+        }
+    }
+
+    private static void userMenu() {
+        while (true) {
+            System.out.println("------абитуриент------");
+            System.out.println("Выберите действие: ");
+            System.out.println("[0] - выход");
+
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            switch (input){
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+            }
+            if ("0".equals(input)) {
+                break;
+            }
+        }
+    }
+
+
     public static void main(String [] args) {
         UserType userType = getUserType();
+
+
+        switch (userType) {
+            case user:
+                userMenu();
+                break;
+            case administrator:
+                administratorMenu();
+                break;
+        }
     }
 
 }
