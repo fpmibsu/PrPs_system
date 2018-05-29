bdc22d458c2b2d6562203337e6190f9a34d33d62
Ilya Sysoi <14ilya12322@gmail.com>
2018-05-29 22:36:44 +0300

    add properties to model

--- src/Models/FacultyInfo.java
+++ src/Models/FacultyInfo.java
@@ -1,4 +1,23 @@
 package Models;
 
 public class FacultyInfo {
+
+    private int code;
+    private String name;
+    private String shortName;
+    private HeadInfo[] heads;
+    private SpecialityInfo[] spec;
+
+    public FacultyInfo(int code,
+                       String name,
+                       String shortName,
+                       HeadInfo[] heads,
+                       SpecialityInfo[] spec) {
+        this.code = code;
+        this.name = name;
+        this.shortName = shortName;
+        this.heads = heads;
+        this.spec = spec;
+
+    }
 }
