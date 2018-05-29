c7fa1d8acda7c050d5b2214c8129f0a6f3c0cddc
Ilya Sysoi <14ilya12322@gmail.com>
2018-05-29 22:40:38 +0300

    add properties to model

--- src/Models/EducationUnit.java
+++ src/Models/EducationUnit.java
@@ -1,4 +1,31 @@
 package Models;
 
 public class EducationUnit {
+
+    private int code;
+    private String name;
+    private String shortName;
+    private HeadInfo[] heads;
+    private SpecialityInfo[] spec;
+    private String address;
+    private String telephone;
+    private String site;
+
+    public EducationUnit(int code,
+                         String name,
+                         String shortName,
+                         HeadInfo[] heads,
+                         SpecialityInfo[] spec,
+                         String address,
+                         String telephone,
+                         String site) {
+        this.code = code;
+        this.name = name;
+        this.shortName = shortName;
+        this.heads = heads;
+        this.spec = spec;
+        this.address = address;
+        this.telephone = telephone;
+        this.site = site;
+    }
 }
