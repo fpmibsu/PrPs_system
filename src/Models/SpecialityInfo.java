6902d90df470cc411d0e9e238e31b5d8a34f312b
Ilya Sysoi <14ilya12322@gmail.com>
2018-05-29 22:30:14 +0300

    add properties to model

--- src/Models/SpecialityInfo.java
+++ src/Models/SpecialityInfo.java
@@ -1,4 +1,22 @@
 package Models;
 
 public class SpecialityInfo {
+
+    private int code;
+    private String name;
+    private String shortName;
+
+    private Boolean isDaily;
+
+    public SpecialityInfo(int code,
+                          String name,
+                          String shortName,
+                          Boolean isDaily) {
+        this.code = code;
+        this.name = name;
+        this.shortName = shortName;
+        this.isDaily = isDaily;
+
+    }
 }
+
