14593a709aa872aa2ca8b63ab2d961b9b0b112df
Ilya Sysoi <14ilya12322@gmail.com>
2018-05-30 12:15:36 +0300

    add new test and fix others

--- src/Test/TestSystem.java
+++ src/Test/TestSystem.java
@@ -7,8 +7,8 @@ public class TestSystem extends Assert {
     @Test
     public void TestPulpit() {
 
-        HeadInfo[] heads = HeadInfo[4];
-        SpecialityInfo[] specs = SpecialityInfo[4];
+        HeadInfo[] heads = new HeadInfo[4];
+        SpecialityInfo[] specs = new SpecialityInfo[4];
 
         EducationUnit item = new EducationUnit(12,
         "Test",
@@ -23,10 +23,23 @@ public class TestSystem extends Assert {
 
     @Test
     public void HeadInfo() {
-        HeadInfo heads = HeadInfo("fio", "PhD", "2015");
+        HeadInfo heads = new HeadInfo("fio", "PhD", "2015");
         assertEquals(item.getFIO(), "fio", "The fio the same");
         assertEquals(item.getDegree(), "PhD", "The degree the same");
         assertEquals(item.getYears(), "2015", "The years the same");
+    }
+
+    @Test
+    public void TestFaculties() {
+        FacultyInfo[] tt =  new FacultyInfo[3];
+
+        assertEquals(tt), new FacultyInfo[3];, "The Faculties the same");
+    }
+
+    @Ignore
+    @Test
+    public void todo1() {
+       
     }
 
     @Ignore
