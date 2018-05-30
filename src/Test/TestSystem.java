28be595b49d7810fd2dadafe6b24e3fc8cb3240a
Ilya Sysoi <14ilya12322@gmail.com>
2018-05-30 10:05:07 +0300

    add test mock

---
+++ src/Test/TestSystem.java
@@ -0,0 +1,42 @@
+package Test;
+import org.junit.*;
+
+public class TestSystem extends Assert {
+
+    /*
+    @Before
+    public void init() { math = new MathFunc(); }
+    @After
+    public void tearDown() { math = null; }
+
+    @Test
+    public void calls() {
+        assertEquals(0, math.getCalls());
+
+        math.factorial(1);
+        assertEquals(1, math.getCalls());
+
+        math.factorial(1);
+        assertEquals(2, math.getCalls());
+    }
+
+    @Test
+    public void factorial() {
+        assertTrue(math.factorial(0) == 1);
+        assertTrue(math.factorial(1) == 1);
+        assertTrue(math.factorial(5) == 120);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void factorialNegative() {
+        math.factorial(-1);
+    }
+
+    @Ignore
+    @Test
+    public void todo() {
+        assertTrue(math.plus(1, 1) == 3);
+    }
+    */
+
+}
